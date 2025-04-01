4cdfd556e45cda978aaad909ad5544634178cee8
Sergey Pavlov <sergey.pavlov@dsr-corporation.com>
2025-04-01 15:38:43 +0300

    add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,88 @@
 import { Component } from '@angular/core';
-import { TableComponent } from './table';
+import { TableComponent, TColumn, TRow } from './table';
+
+const ROWS: TRow[] = [
+  [
+    { key: "id", value: 1 },
+    { key: "name", value: "Alice" },
+    { key: "date", value: new Date() },
+    { key: "is_active", value: true },
+    { key: "metadata", value: 'asdqweq' },
+    { key: "stock", value: null }
+  ],
+  [
+    { key: "id", value: 2 },
+    { key: "name", value: "123" },
+    { key: "date", value: new Date() },
+    { key: "is_active", value: true },
+    { key: "metadata", value: 'asdqweq' },
+    { key: "stock", value: 'stock' }
+  ],
+  [
+    { key: "id", value: 145 },
+    { key: "name", value: "Laptop" },
+    { key: "date", value: new Date() },
+    { key: "is_active", value: true },
+    { key: "metadata", value: 'asd' },
+    { key: "stock", value: 'stock val' }
+  ],
+  [
+    { key: "id", value: 145 },
+    { key: "name", value: "Lapptop" },
+    { key: "date", value: new Date() },
+    { key: "is_active", value: true },
+    { key: "metadata", value: 'asd' },
+    { key: "stock", value: 'stock val' }
+  ],
+
+  [
+    { key: "id", value: 146 },
+    { key: "name", value: "Laptop" },
+    { key: "date", value: new Date() },
+    { key: "is_active", value: true },
+    { key: "metadata", value: 'asd' },
+    { key: "stock", value: null }
+  ],    
+  [
+    { key: "id", value: 175 },
+    { key: "name", value: "Laptop" },
+    { key: "date", value: new Date() },
+    { key: "is_active", value: true },
+    { key: "metadata", value: 'asd' },
+    { key: "stock", value: null }
+  ],
+];
+
+const COLUMNS: TColumn[]  = [
+  {
+    key: 'id',
+    title: 'id',
+    type: 'number'
+  },
+  {
+    key: 'name',
+    title: 'name',
+    type: 'string'
+  },
+  {
+    key: 'date',
+    title: 'date',
+    type: 'date'
+  },
+  {
+    key: 'is_active',
+    title: 'is_active',
+    type: 'boolean',
+  },
+  {
+    key: 'metadata',
+    title: 'metadata',
+  },
+  {
+    key: 'stock',
+    title: 'stock',
+  },
+] 
 
 @Component({
   selector: 'app-root',
@@ -8,4 +91,6 @@ import { TableComponent } from './table';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  columns = COLUMNS;
+  rows = ROWS;
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,11 +1,153 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { getFormattedRows, getRowValue } from './lib/utils';
+import { TColumn, TRow, TSort } from './lib/types';
+import { defaultSort } from './lib/constants';
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
+  @Input() columns: TColumn[] = []
+  @Input() rows: TRow[] = []
+  @Input() checked: boolean = false
 
+
+  copyRows: TRow[] = []
+  isShowFilters: boolean = true;
+
+  sort: TSort = defaultSort
+
+  formattedRows: TRow[] = []
+  filters!: FormGroup;
+
+  constructor() {}
+
+  initForm = () => {
+    const additionalFilters = {
+      showOnlySelected: new FormControl(false)
+    }
+
+    const filters = this.columns.reduce((acc, el) => {
+      return {
+        ...acc,
+        [el.key]: new FormControl()
+      }
+    }, additionalFilters)
+
+    this.filters = new FormGroup(filters)
+  }
+
+  ngOnInit() {
+    debugger
+    this.initForm()
+    this.formattedRows = getFormattedRows(this.rows)
+    this.copyRows = getFormattedRows(this.rows)
+  }
+
+
+  onResetFilters = () => {
+    this.filters.reset();
+    this.filters.value.showOnlySelected = false
+
+    this.formattedRows = this.copyRows
+  } 
+
+  onShowOnlySelectedClick = () => {
+    this.filters.value.showOnlySelected = !this.filters.value.showOnlySelected
+
+    if(this.filters.value.showOnlySelected) {
+      const selectedFilterWorker = new Worker(new URL('./lib/selectedFilter.worker', import.meta.url), { type: 'module' })
+      selectedFilterWorker.postMessage({ rows: this.formattedRows, filters: this.filters.value })
+      selectedFilterWorker.onmessage = ({ data }) => {
+        this.formattedRows = data
+      };
+    } else {
+      const newRows = this.copyRows.map((row) => {
+        const f = this.formattedRows.find((fr) => {
+          const rowFromCopy = row.find((fn) => fn.key === 'id')
+          const currRow = fr.find((r1) => r1.key === 'id')
+          return  rowFromCopy?.value === currRow?.value
+        })
+
+        if(f) return f
+        return row
+      })
+      this.formattedRows = newRows
+    }
+  }
+
+  onChangeInput = () => {
+    const sortWorker = new Worker(new URL('./lib/filter.worker', import.meta.url), { type: 'module' })
+
+    sortWorker.postMessage({ rows: this.formattedRows, filters: this.filters.value })
+
+    sortWorker.onmessage = ({ data }) => {
+      this.formattedRows = data
+    };
+  }
+
+  onSetSort = (e: Event) => {
+    const target = e.target as HTMLElement;
+    const thElement = target.closest('th');
+    
+    if (!thElement) return;
+  
+    const columnKey = thElement.dataset['columnKey'];
+    
+    if (columnKey) {
+      if(columnKey === this.sort.key){
+        this.sort = {
+          ...this.sort,
+          direction: !this.sort.direction
+        }
+      } else {
+        this.sort = {
+          key: columnKey,
+          direction: true
+        }
+      }
+
+      const sortWorker = new Worker(new URL('./lib/sort.worker', import.meta.url), { type: 'module' })
+
+      sortWorker.postMessage({ rows: this.formattedRows, sortOptions: this.sort })
+
+      sortWorker.onmessage = ({ data }) => {
+        this.formattedRows = data
+      };
+    }
+  }
+
+  onCheckboxChange = (row: TRow) => {
+    const newRows = this.formattedRows.map((r) => {
+      if(r !== row) return r
+    
+      const res = r.map((el) => {
+        if(el.key !== "checkbox") return el
+
+        return {
+          key: 'checkbox',
+          value: !el.value
+        }
+      })
+
+      return res
+      
+    })
+    console.log(newRows)
+    this.formattedRows = newRows
+  }
+
+  getCheckedState = (row: TRow) => {
+    return row.find((r) => r.key === 'checkbox')?.value
+  }
+
+  onHideFilters = () => {
+    this.isShowFilters = !this.isShowFilters
+  }
+
+  getRowValueGetter = getRowValue
 }
